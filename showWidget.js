fea27f1c76151afdb35803f0eb669d08848d84a5
e m <emyr.tomos@gmail.com>
2014-03-19 15:26:31 +0000

    Entire voting sequence except for submit. Values are amended directly on the prediction object, so there is no need to pass along form values just submit from the object.

--- showWidget.js
+++ showWidget.js
@@ -15,6 +15,7 @@ $(document).ready(function(){
 		predictor.templates.fixtureString = document.getElementById('predictor_fixture').fetchTemplate().innerHTML.replace(/(\r\n|\n|\r)/gm,"");
 		predictor.templates.voteString = document.getElementById('predictor_play').fetchTemplate().innerHTML.replace(/(\r\n|\n|\r)/gm,"");
 		predictor.templates.scoreString = document.getElementById('predictor_score').fetchTemplate().innerHTML.replace(/(\r\n|\n|\r)/gm,"");
+		predictor.templates.formString = document.getElementById('predictor_form').fetchTemplate().innerHTML.replace(/(\r\n|\n|\r)/gm,"");
 		predictor.templates.statsString = document.getElementById('predictor_stats').fetchTemplate().innerHTML.replace(/(\r\n|\n|\r)/gm,"");
 		for (var fixtureId in predictor.data.fixtures){
 			if(!predictor.data.myPredictions[fixtureId].voted){
@@ -37,31 +38,64 @@ predictor.createFixture = function(fixtureId){
 	var statsString = eval(predictor.templates.statsString);
 	return containerNode.append(fixtureString).append(voteString).append(statsString);
 }
-predictor.createScoreInvitation = function(resultstring){
-	return 
+predictor.createScoreInvitation = function(fixtureId , chosen){
+		var prediction = predictor.data.myPredictions[fixtureId];
+		prediction.prediction.result = chosen;
+		//var teamString = predictor.data.fixtures[fixtureId][chosen].id;
+		var outgoing = $("#predictor div.play");
+		var resultstring = (chosen === "draw")?"You predict a draw":predictor.data.teamStats[predictor.data.fixtures[fixtureId][chosen].id].fullName + " to win";
+		var scoreString = eval(predictor.templates.scoreString);
+		outgoing.parent().append(scoreString);
+		var incoming = $("#predictor div.score");
+		incoming.find("a.predictor-submit").click(function(event){
+			//code to submit prediction here
+			console.log("SKIPPEEE!!!");
+			return false;
+		});
+		incoming.find("a.predict-score").click(function(event){
+			//code to submit prediction here
+			predictor.createScoreForm(fixtureId , chosen);
+			console.log("Predicting ...");
+			return false;
+		});
+		predictor.switchState(incoming , outgoing);
+		return false;
 }
+predictor.createScoreForm = function(fixtureId , chosen){
+		var outgoing = $("#predictor div.score");
+		var fixture = predictor.data.fixtures[fixtureId];
+		var formString = eval(predictor.templates.formString);
+		outgoing.parent().append(formString);
+		var incoming = $("#predictor div.selectScore");
+		var homeSpinBox = new SpinBox('homeGoals', {'minimum':0 , 'maximum' : 20});
+		var awaySpinBox = new SpinBox('awayGoals', {'minimum':0 , 'maximum' : 20});
+		console.log("SPINBOXES: ",homeSpinBox.input , awaySpinBox);
+		$(homeSpinBox.input).change(function(event){
+			console.log(homeSpinBox.getValue());
+			predictor.data.myPredictions[fixtureId].prediction.homeGoals = homeSpinBox.getValue();
+			return false;
+		})
+		$(awaySpinBox.input).change(function(event){
+			console.log(awaySpinBox.getValue());
+			predictor.data.myPredictions[fixtureId].prediction.awayGoals = awaySpinBox.getValue();
+			return false;
+		})
+		console.log("NODES:" , incoming , outgoing);
+		predictor.switchState(incoming , outgoing);
+		return false;
+}
+predictor.switchState = function(incoming,outgoing){
+	outgoing.animate({left : -outgoing.parent().outerWidth(), opacity : 0});
+	incoming.animate({left : 0 , opacity : 1});
+}
+
 predictor.attachEvents = function(fixtureId){
 	$("#predictor a.statslink").click(function(fixtureId){
 		$("#predictor .stats").slideToggle();
 		return false;
 	});
 	$("#predictor div.play button").click(function(event){
-		var prediction = predictor.data.myPredictions[fixtureId];
-		console.log("PREDICTION: ", prediction);
 		var chosen = event.target.attributes["id"].value.split("_")[0];
-		console.log("CHOSEN IS: " , chosen);
-		prediction.voted = true;
-		prediction.prediction = chosen;
-		var teamString = predictor.data.fixtures[fixtureId][chosen].id;
-		console.log("TEAM ID: ", predictor.data.teamStats[teamString]);
-
-		var outgoing = $("#predictor div.play");
-		var resultstring = (chosen === "draw")?"You predict a draw":predictor.data.teamStats[teamString].fullName + " to win";
-		var scoreString = eval(predictor.templates.scoreString);
-		outgoing.parent().append(scoreString);
-		var incoming = $("#predictor div.score");
-		outgoing.animate({left : -outgoing.parent().outerWidth(), opacity : 0});
-		incoming.animate({left : 0 , opacity : 1});
-		return false;
+		return predictor.createScoreInvitation(fixtureId,chosen);
 	});
 }
